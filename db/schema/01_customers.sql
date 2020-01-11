31f9b50dad8c2c86e48978256234b6c071f37a2e
Anthony Pisani <antoineincognito@hotmail.com>
2020-01-11 14:21:26 -0500

    typo in password

--- db/schema/01_customers.sql
+++ db/schema/01_customers.sql
@@ -5,6 +5,6 @@ DROP TABLE IF EXISTS customers CASCADE;
 CREATE TABLE customers (
   id SERIAL PRIMARY KEY NOT NULL,
   username TEXT NOT NULL,
-  passowrd TEXT NOT NULL,
+  password TEXT NOT NULL,
   cell_number TEXT NOT NULL
 );
