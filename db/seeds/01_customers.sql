bd9277e158fd45f5bdea8550d29252ba37a412b1
Anthony Pisani <antoineincognito@hotmail.com>
2020-01-11 13:09:46 -0500

    filled in seeds for customers

---
+++ db/seeds/01_customers.sql
@@ -0,0 +1,21 @@
+-- Users table seeds here (Example)
+INSERT INTO users (username, password, cell_number) 
+VALUES ('Alice', '123', '647-332-2233');
+
+INSERT INTO users (username, password, cell_number) 
+VALUES ('Bobby', '123', '905-112-9821');
+
+INSERT INTO users (username, password, cell_number) 
+VALUES ('Ricky', '123', '416-552-2323');
+
+INSERT INTO users (username, password, cell_number) 
+VALUES ('Alison', '123', '905-905-4322');
+
+INSERT INTO users (username, password, cell_number) 
+VALUES ('Terrance', '123', '646-555-5011');
+
+INSERT INTO users (username, password, cell_number) 
+VALUES ('Vanessa', '123', '647-111-2222');
+
+INSERT INTO users (username, password, cell_number) 
+VALUES ('Brandon', '123', '416-020-1122');
