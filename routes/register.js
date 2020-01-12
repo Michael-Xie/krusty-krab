888890019d62ff418a15dcd7b122ae9af44ab189
Anthony Pisani <antoineincognito@hotmail.com>
2020-01-11 20:00:54 -0500

    removed an unnecessary line

--- routes/register.js
+++ routes/register.js
@@ -14,16 +14,27 @@ module.exports = (db) => {
   // POST /login - Log-in into valid account
   router.post("/", (req, res) => {
     console.log('Posted Registration Information');
+
+    // [TODO] sanitize input 
     const username = req.body.username
     const sms      = req.body.cellNumber
+
+    // call verify username from our models.
     register.verifyUsername(username)
-      .then(result => { 
-        console.log(result)
-      })
-      .catch(err => console.log(err))
-    register.verifySMS(sms)
       .then(result => {
-        console.log(result)
+        if (!result) {
+          register.verifySMS(sms)
+            .then(result => {
+              if (!result) {
+                // handle re-routing to orders.
+                console.log(result)
+              }
+            })
+            .catch(err => console.log(err))
+        } else {
+          // tell user username/sms is already taken.
+          console.log("TAKEN!")
+        }
       })
       .catch(err => console.log(err))
     return;
