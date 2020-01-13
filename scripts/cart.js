3d34317d08624554c65e1ff6aa5979bfe3f550b4
Anthony Pisani <antoineincognito@hotmail.com>
2020-01-13 13:21:10 -0500

    implemented cart-counter

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   $(".item-btn").click(function(event) {
     // get the id of the .item-btn parent element and check the name.
-    const $itemBtnId = this.id
+    const $itemBtnId = $(this).parent().attr("id")
+    const $itemPrice = $(this).parent().children(`#${$itemBtnId}-price`).html()
     // check if the .item-btn parent id exists in the cart already.
-    if ($(`#cart-container #${$itemBtnId}-cart`).length) {
-      let $value = $(`#cart-container #${$itemBtnId}-cart-value`).html()
+    if ($(`#item-container #${$itemBtnId}-cart`).length) {
+      let $value = $(`#${$itemBtnId}-cart-value`).html()
       $value = parseInt($value, 10)
       $value++
       $(`#${$itemBtnId}-cart-value`).html($value)      
@@ -14,7 +15,10 @@ $(document).ready(function() {
       const $itemQuantity = $("<span>", {"class": "item-value", "id": `${$itemBtnId}-cart-value`}).html(1)
       $itemDiv.append($itemName)
       $itemDiv.append($itemQuantity)
-      $(`#cart-container`).append($itemDiv)
+      $(`#item-container`).append($itemDiv)
     }
+    // alter the price.
+    let $currentTotal = Number($('#price-total').html()) + (Number($itemPrice) / 100)
+    $('#price-total').html(`${$currentTotal.toFixed(2)}`)
   })
 })
