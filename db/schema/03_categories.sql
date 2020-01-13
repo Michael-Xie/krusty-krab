91826135f95b7ae38e5fef63e52edd3480c48cd1
Anthony Pisani <antoineincognito@hotmail.com>
2020-01-13 17:09:21 -0500

    fixed typo

--- db/schema/03_categories.sql
+++ db/schema/03_categories.sql
@@ -1,6 +1,6 @@
 -- Drop and recreate Widgets table (Example)
 
-DROP TABLE IF EXISTS category CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
 CREATE TABLE categories (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
