d3f118466aa152b00ef63fd9bba187c9d8424074
Anthony Pisani <antoineincognito@hotmail.com>
2020-01-16 10:41:32 -0500

    did not update on last merge... re-committing

--- models/twilioSMS.js
+++ models/twilioSMS.js
@@ -27,16 +27,6 @@ module.exports = (db) => {
         }, orderDuration * 1000)
       })
   }
-    /*
-    const messagingResponse = require('twilio').twiml.MessagingResponse
-
-    app.post('/sms', (req, res) => {
-      const twiml = new MessagingResponse()
-      twiml.message('The Robots are coming! Head for the hills')
-
-      res.writeHead(200, {'Content-Type': 'text/xml'})
-      res.end(twiml.toString())
-    */
 
   return { sendSMS }
 }
