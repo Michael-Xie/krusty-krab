b8bcdd000d8b96954e014abca225b0ca5c1c34a4
Julie Nguyen <julie-nguyen@live.com>
2020-01-16 20:25:19 +0000

    added data

--- db/seeds/04_menu_items.sql
+++ db/seeds/04_menu_items.sql
@@ -1,42 +1,42 @@
 -- Insert values into menu_items.
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Krabby Patty', 125, 'This special burger is a staple of the Krusty Krab. In fact, it is so delicious that we cannot list the contents of the burger for fear that the special formula will fall into evil hands!', 10000, 'https://vignette.wikia.nocookie.net/spongebob/images/d/d4/Krabby_Patty_transparentpng.png/revision/latest?cb=20170310181007', 1);
+VALUES ('Krabby Patty', 125, 'This special burger is a staple of the Krusty Krab. In fact, it is so delicious that we cannot list the contents of the burger for fear that the special formula will fall into evil hands!', 10000, '../images/krabby-patty.jpg', 1);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Double Krabby Patty', 150, '... temp desc.', 12000, 'blank', 1);
+VALUES ('Double Krabby Patty', 150, 'This is a double special burger that is a double staple of the Krusty Krab. In fact it is so double delicious that we cannot list the contents of the burger for double fear that our double special formula will fall into evil hands!', 12000, '../images/double-crabby-patty.jpg', 1);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Triple Krabby Patty', 200, '... temp desc.', 12000, 'blank', 1);
+VALUES ('Triple Krabby Patty', 200, 'This is a triple special burger that is a triple staple of the Krusty Krab. In fact it is so triple delicious that we cannot list the contents of the burger for triple fear that our triple special formula will fall into evil hands!', 12000, '../images/triple-krabby-patty.png', 1);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Coral Bits', 125, '... temp desc.', 5000, 'blank', 3);
+VALUES ('Coral Bits', 125, 'Enjoy a light snack with the healthy, delicious, fantastic, beautiful, radiant, perfect, Coral Bits', 5000, '../images/coral-bits.png', 3);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Kelp Rings', 150, '... temp desc.', 8000, 'blank', 3);
+VALUES ('Kelp Fries', 150, 'Enjoy a heavier snack with the not healthy, but delicious and fantastic and beautiful and radiant and perfect, Kelp Fries', 8000, '../images/kelp-fries.jpg', 3);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Krabby Meal', 350, '... temp desc.', 15000, 'blank', 2);
+VALUES ('Krabby Meal', 350, 'If you love the special Krabby Patty, you can enjoy it here with our deicious juice and Kelp Fries! Comes with a toy, so there will be everylasting fun!!!', 15000, '../images/krabby-patty-meal.jpg', 2);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Double Krabby Meal', 375, '... temp desc.', 18000, 'blank', 2);
+VALUES ('Double Krabby Meal', 375, 'If you love the double special double Krabby Patty, you can double enjoy it here with our double deicious juice and Kelp Fries! Comes with a toy, so there will be double everylasting fun!!!', 18000, '../images/krabby-patty-meal.jpg', 2);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Triple Krabby Meal', 400, '... temp desc.', 21000, 'blank', 2);
+VALUES ('Triple Krabby Meal', 400, 'If you love the triple special triple Krabby Patty, you can triple enjoy it here with our triple deicious juice and Kelp Fries! Comes with a toy, so there will be triple everylasting fun!!!', 21000, '../images/krabby-patty-meal.jpg', 2);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Salty Sea Dog', 125, '... temp desc.', 8000, 'blank', 5);
+VALUES ('Salty Sea Dog', 125, 'Tired of the Krabby Patty? Of course not! But enjoy your Krabby Patty with a scrumptious Salty Sea Dog (not sea dogs were harmed in the making of this dish).', 8000, '../images/salty-sea-dog.png', 5);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Footlong', 200, '... temp desc.', 8000, 'blank', 5);
+VALUES ('Footlong', 200, 'This footlong is made with the best meat known to the Bikini Bottom! What is this meat? Eat it to find out!', 8000, '../images/footlong.jpg', 5);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Sailors Sunrise', 300, '... temp desc.', 15000, 'blank', 5);
+VALUES ('Sailors Sunrise', 300, 'Want something a little more sweet? Well we got the Sailors Sunrise for you. Enjoy this delicious sunrise with your best friend!', 15000, '../images/sailor-sunrise.jpg', 5);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Golden Loaf', 200, '... temp desc.', 8000, 'blank', 5);
+VALUES ('Golden Loaf', 200, 'This is a golden loaf, THE Golden Loaf. Gold is always good.', 8000, '../images/golden-loaf.jpg', 5);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Kelp Shake', 200, '... temp desc.', 6000, 'blank', 4);
+VALUES ('Kelp Shake', 200, 'One of the best shakes out there for your health! Do not underestimate the powers of the Kelp Shake, it will work wonders.', 6000, '../images/kelp-shake.png', 4);
 
 INSERT INTO menu_items (name, price, description, cook_time_millisec, image_url, category_id)
-VALUES ('Seafoam Soda', 200, '... temp desc.', 5000, 'blank', 4);
+VALUES ('Seafoam Soda', 200, 'Everyone loves a good soda to wash down their Krabby Patty, try the only Seafoam Soda in Bikini Bottom!', 5000, '../images/seafoam-soda.jpg', 4);
